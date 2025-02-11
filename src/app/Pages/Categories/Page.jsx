ed2f6d0f2b3c30b1e6fb7d5fcc5c8b57d9effc8c
Dev.Mohamed <102235955+MohamedAbobakr12@users.noreply.github.com>
2025-02-11 12:52:19 +0200

    Update Page.jsx

--- src/app/Pages/Categories/Page.jsx
+++ src/app/Pages/Categories/Page.jsx
@@ -13,12 +13,12 @@ const Categories = () => {
                 </div>
 
                 <div className="flex max-lg:flex-col max-xl:flex-wrap justify-around max-w-6xl max-xl:max-w-5xl max-lg:w-max max-lg:gap-y-8 h-max mx-auto mt-16">
-                    <div className="flex flex-col justify-center w-[33rem] max-xl:w-[30rem] max-md:w-[28rem] max-lg:h-[35rem] max-md:mx-auto bg-white rounded-lg">
+                    <div className="flex flex-col justify-center w-[33rem] max-xl:w-[30rem] max-md:w-[25rem] max-lg:h-[35rem] max-md:mx-auto bg-white rounded-lg">
                         <h1 className="text-4xl font-semibold text-center text-black mt-2">KIDS BIKES</h1>
                         <p className="text-center text-gray-900 mt-2">Close-out pricing on dozens of products</p>
-                        <div className="flex justify-center w-max mt-2 mx-auto">
+                        <div className="flex items-center justify-center w-max mt-2 mx-auto">
                             <Image 
-                                width={450} 
+                                width={400} 
                                 height={300} 
                                 src={"/bike3.png"} 
                                 alt={"bike3 image"} 
@@ -28,36 +28,36 @@ const Categories = () => {
                         <Link href={"#"} className="-mt-4 mx-auto"><span className="text-xl font-semibold bg-brand-600 hover:bg-white text-white hover:text-brand-600 rounded-md border-2 hover:border hover:border-brand-600 transition-all duration-300 px-16 py-4">Discover More</span></Link>
                     </div>
 
-                    <div className="flex flex-col gap-y-5 w-[33rem] max-xl:w-[30rem] max-md:w-[28rem] h-max">
+                    <div className="flex flex-col gap-y-5 w-[33rem] max-xl:w-[30rem] max-md:w-[26rem] h-max">
                         <div className="flex justify-around border-2 border-white rounded-md h-[50%] bg-white w-full">
-                            <div className="flex flex-col mt-8 w-[50%] h-max">
-                                <h1 className="text-2xl text-center font-semibold">ACCESSOORIES</h1>
-                                <p className="text-lg max-xl:text-sm text-gray-900 w-[75%] mt-5 self-center">Close-out pricing on dozens of products</p>
-                                <Link href={"#"} className="text-brand-600 hover:text-white border-2 border-brand-600 hover:bg-brand-600 w-max rounded px-14 max-xl:px-10 py-4 mx-auto mt-6 transition-all duration-300"><span className="text-xl">Shop Now</span></Link>
+                            <div className="flex flex-col max-md:ml-4 mt-8 max-md:mt-5 w-[50%] h-max">
+                                <h1 className="text-xl text-center font-semibold">ACCESSOORIES</h1>
+                                <p className="text-lg text-center max-xl:text-sm text-gray-900 w-[85%] max-md:w-full mt-5 max-md:mt-3 self-center">Close-out pricing on dozens of products</p>
+                                <Link href={"#"} className="text-brand-600 hover:text-white border-2 border-brand-600 hover:bg-brand-600 w-max rounded px-14 max-xl:px-10 max-md:px-8 py-4 mx-auto mt-6 max-md:mt-3 max-md:mb-4 transition-all duration-300"><span className="text-xl">Shop Now</span></Link>
                             </div>
-                            <div className="w-max grid">
+                            <div className="flex justify-end items-center w-max">
                                 <Image 
                                     width={270} 
                                     height={200} 
                                     src={"/chainstay.png"} 
                                     alt={"chainstay image"} 
-                                    className="rounded-e-md"
+                                    className="rounded-e-md max-md:w-[80%] max-md:h-[100%]"
                                 />
                             </div>
                         </div>
                         <div className="flex justify-around border-2 border-white rounded-md h-[50%] bg-white w-full">
-                            <div className="flex flex-col mt-8 w-[50%] h-max">
-                                <h1 className="text-2xl text-center font-semibold">ACCESSOORIES</h1>
-                                <p className="text-lg max-xl:text-sm text-gray-900 w-[75%] mt-5 self-center">Close-out pricing on dozens of products</p>
-                                <Link href={"#"} className="text-brand-600 hover:text-white border-2 border-brand-600 hover:bg-brand-600 w-max rounded px-14 max-xl:px-10 py-4 mx-auto mt-6 transition-all duration-300"><span className="text-xl">Shop Now</span></Link>
+                            <div className="flex flex-col max-md:ml-4 mt-8 max-md:mt-5 w-[50%] h-max">
+                                <h1 className="text-xl text-center font-semibold">ACCESSOORIES</h1>
+                                <p className="text-lg text-center max-xl:text-sm text-gray-900 w-[85%] max-md:w-full mt-5 max-md:mt-3 self-center">Close-out pricing on dozens of products</p>
+                                <Link href={"#"} className="text-brand-600 hover:text-white border-2 border-brand-600 hover:bg-brand-600 w-max rounded px-14 max-xl:px-10 max-md:px-8 py-4 mx-auto mt-6 max-md:mt-3 max-md:mb-4 transition-all duration-300"><span className="text-xl">Shop Now</span></Link>
                             </div>
-                            <div className="w-max grid">
+                            <div className="flex justify-end items-center w-max">
                                 <Image 
                                     width={270} 
                                     height={200} 
                                     src={"/chainstay.png"} 
                                     alt={"chainstay image"} 
-                                    className="rounded-e-md"
+                                    className="rounded-e-md max-md:w-[80%] max-md:h-[100%]"
                                 />
                             </div>
                         </div>
