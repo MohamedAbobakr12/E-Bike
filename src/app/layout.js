7d506a54a2df2870724b99a19eed474d9fcfc068
Dev.Mohamed <102235955+MohamedAbobakr12@users.noreply.github.com>
2025-02-12 17:52:45 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${poppins.className} antialiased`}
+        className={`${poppins.className} antialiased overflow-x-hidden`}
       >
         {children}
       </body>
